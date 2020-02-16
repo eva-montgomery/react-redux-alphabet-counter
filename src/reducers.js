7661948a3f78157b2eb015043e15f65048be0418
eva montgomery <eva.m.montgomery@gmail.com>
2020-02-16 12:23:03 -0500

    adds updated reducer functions

--- src/reducers.js
+++ src/reducers.js
@@ -5,27 +5,32 @@ import {
 } from "./actions"
 
 
-export function letters(state=11, action) {
+export function letters(state="m", action) {
     const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     let newState = state;
 
     switch(action.type) {
         case INCREMENT:
-            for (let i = 0; i < alphabet.length; i++) {
-                if (alphabet.length > 25) { 
-                    return alphabet.lengths - 25
-                }
-            } break;
+            let letterIndexIncrement = alphabet.indexOf(state)
+            letterIndexIncrement += 1
+            if (letterIndexIncrement === alphabet.length) {
+                letterIndexIncrement = 0
+            }
+            mewState = alphabet[letterIndexIncrement]
+            break;
         case DECREMENT:
-            for (let i = 25; i < alphabet.length;  i--) {
-                if (alphabet.length < 0) { 
-                    return alphabet.lengths - 25
-                }
-
-            } break;
+            let letterIndexDecrement= alphabet.indexOf(state)
+            letterIndexDecrement -= 1
+            if (letterIndexDecrement < 0) {
+                letterIndexDecrement = alphabet.length -1
+            }
+            mewState = alphabet[letterIndexDecrement]
+            break;
 
         case RESET:
-            newState = 11;
+            newState = "m";
+            break;
     }
-    return newState;
+    
+    return (newState)
 }
